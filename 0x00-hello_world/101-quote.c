530289a76032662c4a08035e70009bd83d97efb0
abidemi58 <abidemiseyi164real@gmail.com>
2022-06-30 16:21:06 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include<unistd.h>
+/**
+ * main- prints "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error
+ * Return:always 1(success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",58);
+	return(1);
