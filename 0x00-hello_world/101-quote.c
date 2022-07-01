d12f14278e6f508ae942ae05cb8a74b5b00af774
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-01 04:03:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<unistd.h>
 /**
  *main - Prints out the last part of a quote in the standard err *or.
- *Return: always 0
+ *Return: always 1(success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19", 59);
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
