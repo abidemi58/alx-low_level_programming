e3d569d1d0051455e13f2ce458c817ba7c90c3ef
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-01 19:00:57 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,24 +5,23 @@
  */
 int main(void)
 {
-	int a, b;
+	int num1, num2;
 
-	for (a = 24; a <= 59; a++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (b = 25; b <= 60; b++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			if (b > a)
-			{
-				putchar(a);
-				putchar(b);
-				if (a != 59 || b != 60)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+
+			if (num1 == 8 && num2 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
 			}
 		}
-	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+
+		return (0);
 }
