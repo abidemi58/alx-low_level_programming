41317576a41ae0c7a7b0ad271cf4e507413db3b9
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-01 17:55:25 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,11 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 8; number++)
+	for (number = 0; number <= 9; number++)
 	{
 		putchar((number % 10) + '0');
-		if (number == 8)
+		if (number == 9)
+			continue;
 
 		putchar(',');
 		putchar(' ');
