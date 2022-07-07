f84c2ce8386abd2334431f04e4c1349cd0c426bf
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-07 16:38:00 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,16 @@ void print_line(int n)
 		if (n <= 0)
 		{
 			_putchar('\n');
-		} 
+		}
 		else
-			int j;
 		{
+			int j;
+
 			for (j = 1; j <= n; j++)
+			{
 
 			_putchar('_');
+			}
 		}
 		_putchar('\n');
 
