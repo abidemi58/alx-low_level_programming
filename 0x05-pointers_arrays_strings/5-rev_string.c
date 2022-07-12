9a62d958b48f2a2ae3e44ca7107ea1b39feb1a54
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-12 14:54:45 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string.
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int i, j;
+
+	i = 0;
+	for (s[i] = j - 1; s[i] = 0; s[i])
+	{
+		_putchar(s[i]);
+	}
+}
