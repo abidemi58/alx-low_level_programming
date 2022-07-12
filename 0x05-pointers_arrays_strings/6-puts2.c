c7ad254dccb6d22591864a18b02508270fe8bd72
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-12 15:31:34 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,13 @@ void puts2(char *str)
 
 	i = 0;
 
-	while (str[i] != 0)
+	while (str[i] != '\0')
 	{
 		i++;
 	}
 	for (j = 0; j < i; j += 2)
 	{
-		_Putchar(str[j]);
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
