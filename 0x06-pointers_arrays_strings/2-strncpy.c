b35393b50616db600f604a019c16b3ab64f9b22c
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-14 10:08:12 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,15 +4,15 @@
  * @dest:pointer to destination of the array
  * @src:  string to be appended.
  * @n:maximum number of characters to be appended.
- * Return: a pointer to the resulting string 
+ * Return: a pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (scr[i] != '\0' && i < n)
+	while (src[i] != '\0' && i < n)
 	{
-		dest[i]  = scr[i];
+		dest[i]  = src[i];
 		i++;
 	}
 	while (i < n)
