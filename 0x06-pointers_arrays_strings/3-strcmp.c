a26c0bae6bf78c1e92609b00817b1646bf80fc62
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-14 11:48:20 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int end = 0;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 == '\0')
 		{
-			end = 1;
-			break;
+			return (0);
 		}
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 
