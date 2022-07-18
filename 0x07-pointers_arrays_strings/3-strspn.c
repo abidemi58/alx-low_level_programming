e1d1508b64ef4bf6f2323003ac8567fde861092b
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-18 14:17:12 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,13 +14,13 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-
+		tag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				k++;
-
+				tag = 1
 			}
 		}
 		if (tag == 0)
