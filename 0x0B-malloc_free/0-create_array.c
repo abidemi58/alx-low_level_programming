a4e83f301cb13cc6f1ebaddc3ba0bd998e158617
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-25 15:30:49 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,14 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	s = malloc(sizeof(char) * size);
+	s = (char *) malloc(sizeof(char) * size);
 	if (s == NULL)
 		return (0);
 
 	for (i = 0; i < size; i++)
+	{
 		s[i] = c;
+	}
 
 		return (s);
 }
