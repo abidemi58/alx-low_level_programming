d835b9d1a3cd60a6b14ed150a15f7d42ca05ba74
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-25 16:18:33 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,10 +15,11 @@ char *_strdup(char *str)
 		return (NULL);
 	for (j = 0; j < str[j]; j++)
 		k++;
-		dup = (char *) malloc(sizeof(char) * (j + 1));
+		dup = (char *) malloc(sizeof(char) * (k + 1));
 
 	if (dup == NULL)
 		return (NULL);
+
 	for (j = 0; str[j]; j++)
 	{
 		dup[j] = str[j];
