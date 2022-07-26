0b7eef8dcf6addbaa25249ac9baf87df4ca3adfe
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-26 09:15:38 -0700

    5-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,8 +17,9 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
+		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
+		len++;
 	}
 
 	str = malloc(sizeof(char) * (len + 1));
