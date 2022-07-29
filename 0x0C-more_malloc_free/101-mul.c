0a6af1c0a5276a023487ace6e75b248739288a84
abidemi58 <abidemiseyi164real@gmail.com>
2022-07-28 17:01:32 -0700

    multiplication

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,13 +10,13 @@
  */
 int is_digit(char *s)
 {
-	int j = 0;
+	int i = 0;
 
-	while (s[j])
+	while (s[i])
 	{
-		if (s[j] < '0' || s[j] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		j++;
+		i++;
 	}
 	return (1);
 }
@@ -29,13 +29,13 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-	int j = 0;
+	int i = 0;
 
-	while (s[j] != '\0')
+	while (s[i] != '\0')
 	{
-		j++;
+		i++;
 	}
-	return (j);
+	return (i);
 }
 
 /**
@@ -57,7 +57,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, j, carry, dig1, dig2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -68,28 +68,28 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (j = 0; j <= len1 + len2; j++)
-		result[j] = 0;
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		dig1 = s1[len1] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			dig2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (dig1 * dig2);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (j = 0; j < len - 1; j++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[j])
+		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[j] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
