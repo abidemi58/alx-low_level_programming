a01d5057070538742534349f1536590cffa68a92
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-01 14:55:33 -0700

    creation of dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -52,9 +52,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
