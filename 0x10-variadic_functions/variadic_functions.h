78ddcc44887a24934fdabb0d89c16f6b0ceff556
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-05 02:27:55 -0700

    print list of arguments

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,19 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
-
+/**
+ * struct print - a struct type defining what to print
+ * @t: data type
+ * @f: function pointer
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} pri_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
