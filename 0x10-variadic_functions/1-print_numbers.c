cffce28ee6b90aa7b362f36cca7a7a6af5f741ba
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-04 23:29:31 -0700

    printing numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
-		if (i < n && separator != NULL)
+		if (i != 0 && separator != NULL)
 			printf("%s", separator);
 	}
+	printf("\n");
 
 	va_end(list);
 }
