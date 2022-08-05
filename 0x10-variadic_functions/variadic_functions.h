51109e112b05497be2b1ce4337341f05136a73a2
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-05 05:40:56 -0700

    printing all argument

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,6 +12,7 @@ typedef struct print
 	char *t;
 	void (*f)(va_list args);
 } pri_t;
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
