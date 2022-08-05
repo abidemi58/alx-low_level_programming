f3343496fd568e1fc6de255bfffb49ec15a439b0
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-05 00:16:36 -0700

    printing of string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
-
+	}
 	printf("\n");
 
 	va_end(ptr);
