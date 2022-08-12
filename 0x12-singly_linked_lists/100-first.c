1d71b661ec9cb79d4c1b6982a3f6f8d9e910a33b
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-11 19:07:23 -0700

    printung out

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@ void hare(void) __attribute__ ((story));
 void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-        printf("I bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
