8ad9abe4a180ed0f2efb07cfb2e9e0ff5b3b69ee
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-15 11:31:17 -0700

    task 1 to 3

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,10 +2,7 @@
 #define LISTS_H
 
 #include <stdio.h>
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -20,5 +17,9 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif
