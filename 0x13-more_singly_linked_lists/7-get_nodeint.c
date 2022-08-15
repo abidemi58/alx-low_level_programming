2249c4f9da3158078b055b1c05dd05cd84281694
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-15 13:57:10 -0700

    index of nodes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,15 +7,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	 unsigned int i;
+	unsigned int i;
 
-	 for (i = 0; i < index; i++)
-	 {
-		 if (head == NULL)
-			 return (NULL);
-
-		 head = head->next;
-	 }
-	 return (head);
+	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
 }
 
