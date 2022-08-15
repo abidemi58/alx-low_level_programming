0499a43afa9a0743a371445343ffa9271cdf663f
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-15 11:43:45 -0700

    task 1 to 3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,7 +6,7 @@
  * @n: integer for the new node to contain
  * Return: the address of the new element, or NULL if it failed
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	list_t *new_node;
 
