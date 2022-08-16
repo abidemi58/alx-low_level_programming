43b1d076c0688ecc5d9b905e4a27c43dc5bea586
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-16 16:47:55 -0700

    freeing

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,7 +4,7 @@
  * @h: pointer to the first node in the linked list
  * Return: the size of the list that was free’d
  */
-size_t free_listint_safe(listint_t **h);
+size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
 	int num;
