84e948693573fb27566a4ba02f4b0a285fc2aa23
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-16 16:44:24 -0700

    freeing

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: pointer to the first node in the linked list
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h);
+{
+	size_t len = 0;
+	int num;
+	listint_t *tmp;
+
+	if (*h == NULL)
+		return (0);
+
+	while (*h)
+	{
+		num = *h - (*h)->next;
+		if (num > 0)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (len);
+}
