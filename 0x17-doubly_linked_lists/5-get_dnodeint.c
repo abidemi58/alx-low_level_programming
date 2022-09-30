c14d88bc23c5145f4e500e6e5a8e88cc35b10c69
Abidemi Oluwaseyi <abidemiseyi164real@gmail.com>
2022-09-30 04:34:24 -0700

    all

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,6 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
+ * @head: pointer to the dlistint_t list
+ * @index: index of the node
+ * Return:
